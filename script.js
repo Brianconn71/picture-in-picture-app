335bc0fff7bad014bc6018890335a345cfa34cfd
Brianconn71 <Brian.connolly71@gmail.com>
2021-02-08 17:02:11 +0000

    finished the application

---
+++ script.js
@@ -0,0 +1,28 @@
+const videoEl = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt the user to select media stream, pass to video element, then play
+async function selectMediaStream(){
+    try{
+        const mediaStream = await await navigator.mediaDevices.getDisplayMedia();
+        videoEl.srcObject = mediaStream;
+        videoEl.onloadedmetadata = () => {
+            videoEl.play();
+        }
+    } catch(err){
+        // catch error
+        console.log(err)
+    }
+}
+
+button.addEventListener('click', async () => {
+    // Disable Buton
+    button.disabled = true;
+    //start picture in picture
+    await videoEl.requestPictureInPicture();
+    //reset button
+    button.disabled = false;
+});
+
+// on load
+selectMediaStream();
